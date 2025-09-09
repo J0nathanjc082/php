d91f2a5154a74a007bb295eea455127dd29621f7
Fortis <6300532@clases.edu.sv>
2025-09-09 10:49:09 -0600

    Update crear.php

--- crear.php
+++ crear.php
@@ -1,25 +1,22 @@
 <?php
-$conexion = new mysqli("db4free.net", "fortis", "S0P0RT3*-", "festival_flores_");
+include "koneksi.php";
 
-if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
-}
-
-$Apellidos     = $_POST['Apellidos'];
-$Nombres       = $_POST['Nombres'];
-$Edad          = $_POST['Edad'];
-$Seccion       = $_POST['Seccion'];
-$Especialidad  = $_POST['Especialidad'];
-$Votos         = $_POST['Votos'];
-$Dinero_recaudado= $_POST['Dinero_recaudado'];
-$Inscripcion= $_POST['fecha_inscripcion']; // viene manual de App Inventor
+$Apellidos        = $_POST['Apellidos'];
+$Nombres          = $_POST['Nombres'];
+$Edad             = $_POST['Edad'];
+$Seccion          = $_POST['Seccion'];
+$Especialidad     = $_POST['Especialidad'];
+$Votos            = $_POST['Votos'];
+$Dinero_recaudado = $_POST['Dinero_recaudado'];
+$Inscripcion      = $_POST['fecha_inscripcion']; // manual desde App Inventor
 
 $sql = "INSERT INTO candidatas (Apellidos, Nombres, Edad, Seccion, Especialidad, Votos, Dinero_recaudado, Inscripcion)
         VALUES ('$Apellidos','$Nombres','$Edad','$Seccion','$Especialidad','$Votos','$Dinero_recaudado','$Inscripcion')";
 
-if ($conexion->query($sql)) {
+if (mysqli_query($koneksi, $sql)) {
     echo "ok";
 } else {
-    echo "error: " . $conexion->error;
+    echo "error: " . mysqli_error($koneksi);
 }
 ?>
+
